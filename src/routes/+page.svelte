a570ec8c8dcd25b296792cf8b40cbac66da56d3d
Eirik Berget Haukeland <eirik@haukeland.name>
2023-03-12 17:26:25 +0100

    upated descriptions of each card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,21 +9,21 @@
 		{
 			img: gameHubImg,
 			title: 'GameHub',
-			description: 'game store on line. text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting ',
+			description: 'game store on line. That fetches data form a wordpress api with functionality to add product to a cart as well as sending in a message',
 			repoLink: 'https://github.com/Noroff-FEU-Assignments/cross-course-project-Eirik-Haukeland',
 			deployedLink: 'https://fantastic-pegasus-1d8ebd.netlify.app/'
 		},
 		{
 			img: hebsRecipesImg,
 			title: "Heb's Recipes",
-			description: 'cooking blog site. text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting ',
+			description: 'cooking blog site. Recipes are added on a wordpress server and fetched on load. This page has the ability to search for recipes and filter recipes by tag which are populated form the recipes.',
 			repoLink: 'https://github.com/Noroff-FEU-Assignments/project-exam-1-Eirik-Haukeland',
 			deployedLink: 'https://joyful-halva-a4d44f.netlify.app/index.html'
 		},
 		{
 			img: scienceImg,
 			title: 'Museum Site',
-			description: 'Community Science Museum. text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting text is texting ',
+			description: 'Community Science Museum. This is a static site that are written in pure javascript, Html and Css.',
 			repoLink: 'https://github.com/Eirik-Haukeland/Semester-Project-1',
 			deployedLink: 'https://brilliant-blancmange-0b76a9.netlify.app/'
 		}
